3945fdd68353b715eb9bf1635863cdb9da9ae5e5
Fabian <fabian@familie-palitza.de>
2018-01-11 21:34:32 +0100

    Test - Banana.java überschrieben

--- src/Banana.java
+++ src/Banana.java
@@ -1,9 +1,22 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Banana {
+    private int gelb;
+    
+    public Banana() {
+        this.gelb = 5;
+    }
+    
+   // @override
+    public String toString() {
+        return "Diese Banane ist genau " + gelb + " gelb.";
+    }
     public static void main(String[] args) {
-        System.out.println("hi");
-        List<String> ban = new BananaList<>();
+        ArrayList <Banana> al = new ArrayList<Banana>();
+        al.add(new Banana());
+        al.add(new Banana());
+        for (Banana el : al.toArray(new Banana[1])) {
+            System.out.println(el);
+        }   
     }
 }
